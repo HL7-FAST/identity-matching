6bd45b721c68393b484500fffd5878ac796d86b7
Adam Phillips <adam.phillips@lantanagroup.com>
2023-04-17 11:11:57 -0400

    Updated getters and setters for DiscoveryObject

--- src/main/java/ca/uhn/fhir/jpa/starter/operations/models/DiscoveryObject.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/operations/models/DiscoveryObject.java
@@ -1,11 +1,7 @@
 package ca.uhn.fhir.jpa.starter.operations.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Getter
-@Setter
 public class DiscoveryObject {
 	private String[] udap_versions_supported;
 	private String[] udap_profiles_supported;
@@ -22,4 +18,50 @@ public class DiscoveryObject {
 	private String registration_endpoint;
 	private String[] registration_endpoint_jwt_signing_alg_values_supported;
 	private String signed_metadata;
+
+	public String[] getUdap_versions_supported() { return udap_versions_supported; }
+	public void setUdap_versions_supported(String[] system) { this.udap_versions_supported = system; }
+
+	public String[] getUdap_profiles_supported() { return udap_profiles_supported; }
+	public void setUdap_profiles_supported(String []system) { this.udap_profiles_supported = system; }
+
+	public String[] getUdap_authorization_extensions_supported() { return udap_authorization_extensions_supported; }
+	public void setUdap_authorization_extensions_supported(String[] system) { this.udap_authorization_extensions_supported = system; }
+
+	public String[] getUdap_authorization_extensions_required() { return udap_authorization_extensions_required; }
+	public void setUdap_authorization_extensions_required(String[] system) { this.udap_authorization_extensions_required = system; }
+
+	public String[] getUdap_certifications_supported() { return udap_certifications_supported; }
+	public void setUdap_certifications_supported(String[] system) { this.udap_certifications_supported = system; }
+
+	public String[] getUdap_certifications_required() { return udap_certifications_required; }
+	public void setUdap_certifications_required(String[] system) { this.udap_certifications_required = system; }
+
+	public String[] getGrant_types_supported() { return grant_types_supported; }
+	public void setGrant_types_supported(String[] system) { this.grant_types_supported = system; }
+
+	public String[] getScopes_supported() { return scopes_supported; }
+	public void setScopes_supported(String[] system) { this.scopes_supported = system; }
+
+	public String getAuthorization_endpoint() { return authorization_endpoint; }
+	public void setAuthorization_endpoint(String system) { this.authorization_endpoint = system; }
+
+	public String getToken_endpoint() { return token_endpoint; }
+	public void setToken_endpoint(String system) { this.token_endpoint = system; }
+
+	public String[] getToken_endpoint_auth_methods_supported() { return token_endpoint_auth_methods_supported; }
+	public void setToken_endpoint_auth_methods_supported(String[] system) { this.token_endpoint_auth_methods_supported = system; }
+
+	public String[] getToken_endpoint_auth_signing_alg_values_supported() { return token_endpoint_auth_signing_alg_values_supported; }
+	public void setToken_endpoint_auth_signing_alg_values_supported(String[] system) { this.token_endpoint_auth_signing_alg_values_supported = system; }
+
+	public String getRegistration_endpoint() { return registration_endpoint; }
+	public void setRegistration_endpoint(String system) { this.registration_endpoint = system; }
+
+	public String[] getRegistration_endpoint_jwt_signing_alg_values_supported() { return registration_endpoint_jwt_signing_alg_values_supported; }
+	public void setRegistration_endpoint_jwt_signing_alg_values_supported(String[] system) { this.registration_endpoint_jwt_signing_alg_values_supported = system; }
+
+	public String getSigned_metadata() { return signed_metadata; }
+	public void setSigned_metadata(String system) { this.signed_metadata = system; }
+
 }
